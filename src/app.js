8b36c5b731a3b0554b675a84818bd7f9f1ab0e90
D1n0sXIX <alejmaman@gmail.com>
2026-05-04 09:32:36 +0200

    Add de ficheros reutilizables de la practica intermedia

--- src/app.js
+++ src/app.js
@@ -3,21 +3,20 @@ import helmet from 'helmet';
 import cors from 'cors';
 import mongoSanitize from 'express-mongo-sanitize';
 import rateLimit from 'express-rate-limit';
-
+import errorHandler from './middleware/error-Handler.js';
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
-
 app.use(rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100
 }));
-
 app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
-
 // TODO: aquí conectaremos las rutas
+app.use(errorHandler);
+
 
 export default app;
